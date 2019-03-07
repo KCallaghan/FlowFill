71a068e5bc2223dad42f097d12761aa56b5bd24a
KCallaghan <kerrylee37@gmail.com>
2019-03-06 22:12:51 -0600

    Updated user inputs

--- run_me.sh
+++ run_me.sh
@@ -1,9 +1,9 @@
-for x in  0.2
+for x in  0.01 0.02
 do
 echo $x
 date > temp_startendtimes_$x.txt
 filename="Sangamon_text_"
-mpirun -np 4 ./test $x Sangamon_15_m.nc $filename$x
+mpirun -np 4 ./test $x Sangamon_15_m.nc 497 600 0.001 $filename$x
 date >> temp_startendtimes_$x.txt
 done
 
